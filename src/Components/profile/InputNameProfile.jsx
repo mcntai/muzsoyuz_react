d8107da6e59933d3068fbc0fc410b931f15534be
mcntai <makentai@icloud.com>
2021-01-02 03:04:21 +0200

    - New Profile CSS refactoring v0.4

--- src/Components/profile/InputNameProfile.jsx
+++ src/Components/profile/InputNameProfile.jsx
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { MuzSoyuzRequest } from '../../muzsoyuz-request'
 import * as swalAlert from '../common/Alerts'
-import edit from '../../Assets/img/edit-icon.svg'
 import s from './InputNameProfile.module.css'
 
 
 const InputNameProfile = ({ data }) => {
   const [name, setName] = useState({})
-  const [disabled, setDisabled] = useState(true)
-  const border = !disabled ? s.border : null
   const [finishedUpdatingName, setFinishedUpdatingName] = useState(false)
 
   useEffect(() => {
@@ -34,12 +31,7 @@ const InputNameProfile = ({ data }) => {
     setName(e.target.value)
   }
 
-  const enableNameEdit = () => {
-    setDisabled(false)
-  }
-
   const disableNameEdit = () => {
-    setDisabled(true)
     setFinishedUpdatingName(true)
   }
 
@@ -48,15 +40,13 @@ const InputNameProfile = ({ data }) => {
       <input
         type='text'
         name='name'
-        className={[s.name, s.inp, border].join(' ')}
+        className={[s.name, s.inp].join(' ')}
         value={name || ''}
         placeholder="Твоє ім'я"
         contentEditable={true}
-        disabled={disabled}
         onChange={changeName}
         onBlur={disableNameEdit}
       />
-      <img src={edit} alt='edit' className={s.edit} onClick={enableNameEdit}/>
     </div>
   )
 }
