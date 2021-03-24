55e062c666d5832cfef77eb73de0a1ff205a5a41
mcntai <makentai@icloud.com>
2021-03-24 23:59:43 +0200

    - imageUpload component finish

--- src/components/common/ImageEasyCrop.js
+++ src/components/common/ImageEasyCrop.js
@@ -1,8 +1,8 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react'
+import React, { useState, useCallback, useEffect } from 'react'
 import Cropper from 'react-easy-crop'
-import s from "./ImageEasyCrop.module.css"
 import { useDispatch } from "react-redux"
 import { profileImageUploaded } from "../../slice/general"
+import s from "./ImageEasyCrop.module.css"
 import confirm from "../../assets/img/confirm.svg"
 import remove from "../../assets/img/remove.svg"
 
@@ -12,10 +12,6 @@ const ImageEasyCrop = ({ uploadImageCallback, base64, file }) => {
   const [zoom, setZoom] = useState(1)
   const [completedCrop, setCompletedCrop] = useState()
 
-  // const [imageBlob, setImageBlob] = useState()
-  // const imgRef = useRef(null)
-  // const canvasRef = useRef(null)
-
   const dispatch = useDispatch()
   const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
     setCompletedCrop(croppedAreaPixels)
@@ -24,42 +20,7 @@ const ImageEasyCrop = ({ uploadImageCallback, base64, file }) => {
 
   useEffect(() => {
     setUploadedImage(base64)
-
   }, [base64, uploadedImage])
-
-  // useEffect(() => {
-  //   if (!completedCrop || !imgRef.current) return
-  //
-  //   const image = imgRef.current
-  //   const canvas = canvasRef.current
-  //   const crop = completedCrop
-  //
-  //   const scaleX = image.naturalWidth / image.width
-  //   const scaleY = image.naturalHeight / image.height
-  //   const pixelRatio = window.devicePixelRatio
-  //   const ctx = canvas.getContext("2d")
-  //
-  //   canvas.width = crop.width * pixelRatio
-  //   canvas.height = crop.height * pixelRatio
-  //
-  //   ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0)
-  //   ctx.imageSmoothingQuality = "high"
-  //
-  //   ctx.drawImage(
-  //     image,
-  //     crop.x * scaleX,
-  //     crop.y * scaleY,
-  //     crop.width * scaleX,
-  //     crop.height * scaleY,
-  //     0,
-  //     0,
-  //     crop.width,
-  //     crop.height
-  //   )
-  //
-  //   canvas.toBlob(blob => setImageBlob(blob), "image/jpeg")
-  //
-  // }, [completedCrop])
 
   const fileUploadHandler = () => {
     const fileType = 'jpeg'
@@ -102,10 +63,6 @@ const ImageEasyCrop = ({ uploadImageCallback, base64, file }) => {
           />
         </div>
       </div>
-      {/*<canvas*/}
-      {/*  ref={canvasRef}*/}
-      {/*  style={{ display: "none" }}*/}
-      {/*/>*/}
     </>
   )
 }
