7f0d9e8de398b333c838567815a0eb9bacda4469
mcntai <makentai@icloud.com>
2021-05-11 16:37:13 +0300

    - Google Analytics code v0.4

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import Routes from '../routes/Routes'
 import s from './App.module.css'
-import usePageTracking from "../routes/usePageTracking"
 
 function App() {
-  usePageTracking()
 
   return (
     <div className={s.wrapper}>
--- src/history/history.js
+++ src/history/history.js
@@ -1,6 +1,15 @@
 import { createBrowserHistory } from 'history'
+import ReactGA from "react-ga"
 
+ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_NO)
 
 const history = createBrowserHistory()
+history.listen((location) => {
+  ReactGA.pageview(location.pathname)
+})
+
+if (window.performance && (performance.getEntriesByType("navigation") === performance.getEntriesByType("navigation").TYPE_NAVIGATE)) {
+  ReactGA.pageview("/")
+}
 
 export default history
